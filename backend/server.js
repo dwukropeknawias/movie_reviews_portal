2c50af113bac0c689f43497e3f4452d26cc9e42e
Dawid <pelcuk@gmail.com>
2019-11-14 01:19:34 +0100

    Database connect and models

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,40 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const User = require("./models/user");
+
+//import models from "./models";
+
+app.get("/api/users", function(request, response) {
+  User.findAll().then(users => {
+    response.json(users);
+  });
+});
+
+app.get("/api/users/:id", function(request, response) {
+  let { id } = request.params;
+
+  User.findByPk(id).then(user => {
+    if (user) {
+      response.json(user);
+    } else {
+      response.status(404).send();
+    }
+  });
+});
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: true
   })
 );
 
 app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
 app.listen(port, function() {
   console.log(`Listening at Port ${port}`);
