8d82c108023ba560797b2db261d76615e0e6c1af
Dawid <pelcuk@gmail.com>
2019-11-18 05:03:29 +0100

    Movie Page Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 import Page404 from "./components/Page404";
+import RegistrationPage from "./components/RegistrationPage";
+import MoviePage from "./components/MoviePage";
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +22,8 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route exact path="/registration" component={Page404} />
+            // <Route exact path="/registration" component={RegistrationPage} />
+            <Route exact path="/movies/:MovieId" component={MoviePage} />
             <Route path="*" component={Page404} />
           </Switch>
         </div>
---
+++ frontend/src/components/MoviePage.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import {
+  Header,
+  Grid,
+  Segment,
+  Feed,
+  Image,
+  GridColumn,
+  GridRow,
+  List
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import homePageLogo from "./images/HomePageLogo.png";
+
+import "./MoviePage.css";
+
+class MoviePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movie: {} };
+  }
+
+  componentDidMount() {
+    const {
+      match: { params }
+    } = this.props;
+
+    axios
+      .get(`/api/movies/${params.MovieId}`)
+      .then(response => {
+        this.setState({
+          movie: response.data
+        });
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid textAlign="center">
+          <GridRow>
+            <Segment
+              style={{ height: "40vh", width: "120vh", marginTop: "5.5em" }}
+            >
+              <Header as="h1">Movie info</Header>
+              <Grid columns="4">
+                <GridColumn width="6" />
+
+                <GridColumn width="3" textAlign="left">
+                  <List size="massive">
+                    <List.Item content={"Title: "} />
+                    <List.Item content={"Year: "} />
+                    <List.Item content={"Runtime: "} />
+                    <List.Item content={"Genre: "} />
+                    <List.Item content={"Plot: "} />
+                    <List.Item content={"Awards: "} />
+                    <List.Item content={"Box office: "} />
+                    <List.Item content={"Production: "} />
+                  </List>
+                </GridColumn>
+                <GridColumn textAlign="left">
+                  <List size="massive">
+                    <List.Item content={this.state.movie.title} />
+                    <List.Item content={this.state.movie.year} />
+                    <List.Item content={this.state.movie.runtime} />
+                    <List.Item content={this.state.movie.genre} />
+                    <List.Item content={this.state.movie.plot} />
+                    <List.Item content={this.state.movie.awards} />
+                    <List.Item content={this.state.movie.box_office} />
+                    <List.Item content={this.state.movie.production} />
+                  </List>
+                </GridColumn>
+              </Grid>
+            </Segment>
+          </GridRow>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default MoviePage;
